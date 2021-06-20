4b09c307c9a19149feb1f96695b70c72138d3177
ahmadalrasheed <alrasheedahmad97@gmail.com>
2021-06-20 17:41:39 +0300

    lab01-b result

---
+++ javascript/java.js
@@ -0,0 +1,18 @@
+let username = prompt("what is your name?")
+alert("welcome " +username+" to my website!")
+console.log(username)
+
+
+let city = prompt(username+",where do you go for a vacation?")
+alert("me too " +username+  " i wish to go to  " +city+" its really amazing")
+console.log(city)
+
+
+let food = prompt(username + ",what is the most deleicious food that you eat in " +city+"?" )
+alert("yeah " +username+" i like  "+ food +" very much and really i wish to eat it in " +city+"!!")
+console.log(food)
+
+
+let work = prompt(username + ",what is your job ? ")
+alert("its amazing " +username+ " to work as " +work+", i am so happy to meet you and i wish that one day i will see you in " +city+" and eat "+food+" with you! you are welcome to my website!")
+console.log(work)
